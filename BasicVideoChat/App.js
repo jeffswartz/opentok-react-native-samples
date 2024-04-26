a43291b5192fb63285d8d1619180043f3b9f2459
Jeff Swartz <jeff.swartz@vonage.com>
2024-04-26 14:57:05 -0700

    test 2.25.5 branch

--- BasicVideoChat/App.js
+++ BasicVideoChat/App.js
@@ -6,9 +6,9 @@ import {OTSession, OTPublisher, OTSubscriber} from 'opentok-react-native';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.apiKey = '';
-    this.sessionId = '';
-    this.token = '';
+    this.apiKey = '472032';
+    this.sessionId = '1_MX40NzIwMzJ-fjE3MTQxNjgyMjg3MTZ-ek5ZUE5CaG84YWxtUzdlL3pLUlErTTBwfn5-';
+    this.token = 'T1==cGFydG5lcl9pZD00NzIwMzImc2lnPTE1OWU5OTIyYmJlNDRhYjMwYTU4YTJmYmU4Y2JhZTJlZWEyMTQ1NTA6c2Vzc2lvbl9pZD0xX01YNDBOekl3TXpKLWZqRTNNVFF4TmpneU1qZzNNVFotZWs1WlVFNUNhRzg0WVd4dFV6ZGxMM3BMVWxFclRUQndmbjUtJmNyZWF0ZV90aW1lPTE3MTQxNjgyMjkmbm9uY2U9MC41Njk0MDI1ODQyNzAzMTI1JnJvbGU9bW9kZXJhdG9yJmV4cGlyZV90aW1lPTE3MTY3NjAyMjkmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=';
   }
 
   render() {
