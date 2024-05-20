2ae1ba3470e8d6e065b429c4bd278f86b25a3ffb
Jeff Swartz <jeff.swartz@vonage.com>
2024-05-20 13:02:28 -0700

    Test disable-enableStereoOutput-in-android branch of opentok-react-native
    See the BasicVideoChat sample app.
    
    See https://github.com/opentok/opentok-react-native/pull/751.

--- BasicVideoChat/App.js
+++ BasicVideoChat/App.js
@@ -6,9 +6,11 @@ import {OTSession, OTPublisher, OTSubscriber} from 'opentok-react-native';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.apiKey = '';
-    this.sessionId = '';
-    this.token = '';
+    this.apiKey = '472032';
+    this.sessionId =
+      '2_MX40NzIwMzJ-fjE3MTU3Mjg1OTQ5NTF-M0JRSCt3K1VpbjRhMm9rN0V4Y0hzOUZxfn5-';
+    this.token =
+      'T1==cGFydG5lcl9pZD00NzIwMzImc2lnPWI4MjI2ZDM1ODkyZjk2OTZhM2M3ZDE4M2IzODlkMDkyODFiZWM4OGU6c2Vzc2lvbl9pZD0yX01YNDBOekl3TXpKLWZqRTNNVFUzTWpnMU9UUTVOVEYtTTBKUlNDdDNLMVZwYmpSaE1tOXJOMFY0WTBoek9VWnhmbjUtJmNyZWF0ZV90aW1lPTE3MTU3Mjg1OTUmbm9uY2U9MC40NzQ0MDc4MjI1NzY3NTQ1JnJvbGU9bW9kZXJhdG9yJmV4cGlyZV90aW1lPTE3MTgzMjA1OTUmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=';
   }
 
   render() {
@@ -23,7 +25,8 @@ class App extends Component {
         <OTSession
           apiKey={this.apiKey}
           sessionId={this.sessionId}
-          token={this.token}>
+          token={this.token}
+          options={{enableStereoOutput: true}}>
           <OTPublisher style={{width: 200, height: 200}} />
           <OTSubscriber style={{width: 200, height: 200}} />
         </OTSession>
