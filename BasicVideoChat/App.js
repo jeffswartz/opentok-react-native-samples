0fc371c170c6ebd3e7ba5febe6a6600fda176618
Jeff Swartz <jeff.swartz@vonage.com>
2024-06-24 16:24:37 +0200

    Update BasicVideoChat to use opentok-react-native#v2.28.0

--- BasicVideoChat/App.js
+++ BasicVideoChat/App.js
@@ -6,9 +6,9 @@ import {OTSession, OTPublisher, OTSubscriber} from 'opentok-react-native';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.apiKey = '';
-    this.sessionId = '';
-    this.token = '';
+    this.apiKey = '472032';
+    this.sessionId = '1_MX40NzIwMzJ-fjE3MTkyMjU4NjQ2MzJ-VTgvd3ZZUlRNa1licGRPbUZnSnEyeEp2fn5-';
+    this.token = 'T1==cGFydG5lcl9pZD00NzIwMzImc2lnPTBlMDE2Y2NlYTNiMDYzODYwNTY4ZmY4MGI4NzA5YmEyNjM2MWJiNDE6c2Vzc2lvbl9pZD0xX01YNDBOekl3TXpKLWZqRTNNVGt5TWpVNE5qUTJNekotVlRndmQzWlpVbFJOYTFsaWNHUlBiVVpuU25FeWVFcDJmbjUtJmNyZWF0ZV90aW1lPTE3MTkyMjU4NjUmbm9uY2U9MC4yOTg2NjU5NTQ4NTg4NDAyJnJvbGU9bW9kZXJhdG9yJmV4cGlyZV90aW1lPTE3MjE4MTc4NjUmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=';
   }
 
   render() {
