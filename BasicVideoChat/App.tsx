1c6e68e45c93bb9baa88b59cc0b98e40b5a5ef2b
Jeff Swartz <jeff.swartz@vonage.com>
2024-08-06 17:08:40 -0700

    Add test API key, session ID, and token.

--- BasicVideoChat/App.tsx
+++ BasicVideoChat/App.tsx
@@ -10,9 +10,9 @@ import {
   StreamCreatedEvent,
 } from 'opentok-react-native';
 
-const apiKey = '';
-const sessionId = '';
-const token = '';
+const apiKey = '472032';
+const sessionId = '2_MX40NzIwMzJ-fjE3MjI1NDAwMzMzOTh-UU9yZHIzSjdZMVdCelVac0ZGdytoTFg2fn5-';
+const token = 'T1==cGFydG5lcl9pZD00NzIwMzImc2lnPTNmNzU4MzU3NDU0Yzc2MTIxODkzY2ZlZTgwMzE1M2Q0NjU1NTdhYzc6c2Vzc2lvbl9pZD0yX01YNDBOekl3TXpKLWZqRTNNakkxTkRBd016TXpPVGgtVVU5eVpISXpTamRaTVZkQ2VsVmFjMFpHZHl0b1RGZzJmbjUtJmNyZWF0ZV90aW1lPTE3MjI1NDAwMzMmbm9uY2U9MC42MzEzODE4MDkxNjk5MDA2JnJvbGU9bW9kZXJhdG9yJmV4cGlyZV90aW1lPTE3MjUxMzIwMzMmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=';
 
 class App extends Component {
   private session = createRef<OTSession>();
