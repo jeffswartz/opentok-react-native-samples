28f9fd73fc7286829b2ed36877085328aee079b8
Jeff Swartz <jeff.swartz@vonage.com>
2024-09-17 16:05:20 -0700

    Test OTPublisher event fix in fix-publilsher-streamDestroyed branch

--- BasicVideoChat/App.tsx
+++ BasicVideoChat/App.tsx
@@ -10,33 +10,27 @@ import {
   StreamCreatedEvent,
 } from 'opentok-react-native';
 
-const apiKey = '';
-const sessionId = '';
-const token = '';
+const apiKey = '472032';
+const sessionId =
+  '2_MX40NzIwMzJ-fjE3MjY1OTU2NjIwMDR-TUMzdk9Dd0hPdXdnMnppdjRyRjcxYStafn5-';
+const token =
+  'T1==cGFydG5lcl9pZD00NzIwMzImc2lnPWI5MTZjODRhMzFkMTA4OTFmM2UwNmJjOTc1Mjk0ZjcxNjQ0NDIzNjA6c2Vzc2lvbl9pZD0yX01YNDBOekl3TXpKLWZqRTNNalkxT1RVMk5qSXdNRFItVFVNemRrOURkMGhQZFhkbk1ucHBkalJ5UmpjeFlTdGFmbjUtJmNyZWF0ZV90aW1lPTE3MjY1OTU2OTAmbm9uY2U9MC42NTY0NTg5ODAyNTM5OTQyJnJvbGU9bW9kZXJhdG9yJmV4cGlyZV90aW1lPTE3MjkxODc2ODk5NjUmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=';
 
 class App extends Component {
   private session = createRef<OTSession>();
   private publisher = createRef<OTPublisher>();
   sessionEventHandlers: OTSessionEventHandlers = {
     sessionConnected: async event => {
-      if (this.session.current) {
-        this.session.current.getCapabilities();
-      }
       console.log(
         'session connected -- connection ID:',
         event.connection.connectionId,
       );
-      const capabilities = this.session.current?.getCapabilities().then(() => {
-        console.log('session capabilities:', capabilities);
-      });
     },
   };
 
   publisherEventHandlers: OTPublisherEventHandlers = {
     streamCreated: async (event: StreamCreatedEvent) => {
-      if (this.session.current) {
-        this.session.current.forceMuteAll([event.streamId]);
-      }
+      console.log('OTPublisher streamCreated ID:', event);
     },
   };
 
