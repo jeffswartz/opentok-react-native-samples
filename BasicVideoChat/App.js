dd6ade046ad8ce5385d40636299f1e2994b37fbf
Jeff Swartz <jeff.swartz@vonage.com>
2025-09-03 16:39:02 -0700

    Test connecting to two sessions intermittently

--- BasicVideoChat/App.js
+++ BasicVideoChat/App.js
@@ -9,6 +9,35 @@ class App extends Component {
     this.apiKey = '';
     this.sessionId = '';
     this.token = '';
+    this.sessionId2 = '';
+    this.token2 = '';
+
+    this.state = {
+      connected: true,
+      sessionId: this.sessionId,
+      token: this.token,
+    };
+
+    this.rotateSession = () => {
+      this.setState({
+        connected: false,
+      });
+      setTimeout(() => {
+        if (this.state.sessionId == this.sessionId) {
+          return this.setState({
+            connected: true,
+            sessionId: this.sessionId2,
+            token: this.token2,
+          });
+        }
+        return this.setState({
+          connected: true,
+          sessionId: this.sessionId,
+          token: this.token,
+        });
+        
+      }, 3000);
+    }
   }
 
   render() {
@@ -20,13 +49,22 @@ class App extends Component {
           paddingHorizontal: 100,
           paddingVertical: 50,
         }}>
+        {this.state.connected ?
         <OTSession
           apiKey={this.apiKey}
-          sessionId={this.sessionId}
-          token={this.token}>
+          sessionId={this.state.sessionId}
+          token={this.state.token}
+          eventHandlers={{
+            sessionConnected: (e) => {
+              console.log(e.sessionId);
+              setTimeout(() => this.rotateSession(), 5000);
+            },
+          }}
+        >
           <OTPublisher style={{width: 200, height: 200}} />
           <OTSubscriber style={{width: 200, height: 200}} />
         </OTSession>
+        : null }
       </View>
     );
   }
